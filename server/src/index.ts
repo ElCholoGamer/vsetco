dec4bf36483fc63c5f5a99881abfd62490abf0ba
ElCholoGamer <josedanielgrayson@gmail.com>
2021-03-25 11:09:29 -0500

    add basic express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello, world!');
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('Hello, world!');
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`App listening on port ${PORT}...`));
