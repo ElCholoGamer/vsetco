add6242d06c56eff8a011c284b5168049e70005b
ElCholoGamer <josedanielgrayson@gmail.com>
2021-05-30 19:38:34 -0500

    fix footer padding

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer: React.FC = () => {
 	return (
-		<footer className="bg-secondary py-3 px-1 d-flex align-items-center justify-content-center">
+		<footer className="bg-secondary text-center p-3 d-flex align-items-center justify-content-center">
 			Copyright &copy; 2021 Všetco - Todos los derechos reservados
 		</footer>
 	);
