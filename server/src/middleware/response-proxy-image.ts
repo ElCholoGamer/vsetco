9b2240262f00db5a8f486f8f7aac747b2d9d81e6
ElCholoGamer <josedanielgrayson@gmail.com>
2021-06-14 21:38:45 -0500

    formatting

--- server/src/middleware/response-proxy-image.ts
+++ server/src/middleware/response-proxy-image.ts
@@ -2,25 +2,25 @@ import { RequestHandler } from 'express';
 import axios, { AxiosError } from 'axios';
 
 function responseProxyImage(): RequestHandler {
-    return (req, res, next) => {
-        res.proxyImage = url => {
-            axios.get(url, { responseType: 'arraybuffer'})
-                .then(axiosResponse => {
-                    const { data, headers } = axiosResponse;
-                    res.contentType(headers['content-type']).send(data);
-                })
-                .catch((err: AxiosError) => {
-                    if(err.stack)console.error(err.stack);
+	return (req, res, next) => {
+		res.proxyImage = url => {
+			axios
+				.get(url, { responseType: 'arraybuffer' })
+				.then(axiosResponse => {
+					const { data, headers } = axiosResponse;
+					res.contentType(headers['content-type']).send(data);
+				})
+				.catch((err: AxiosError) => {
+					const status = err.response?.status || 500;
+					res.status(status).json({
+						status,
+						message: err.message || 'Proxy error.',
+					});
+				});
+		};
 
-                    res.json({
-                        status: 500,
-                        message: 'Proxy error.'
-                    })
-                })
-        };
-
-        next();
-    }
+		next();
+	};
 }
 
 export default responseProxyImage;
